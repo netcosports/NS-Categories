a0447677cf3f9fd2b111ceb8f954503a8abf2df2
Anatoly Shcherbinin <anatoly@netcosports.com>
2019-10-08 14:22:57 +0300

    Extend UIButton with image <-> title padding setter

---
+++ Extends/UIButton+NSTCategories.m
@@ -0,0 +1,50 @@
+//
+//  UIButton+NSTCategories.m
+//  NSTCategories
+//
+//  Created by Anatoly Shcherbinin on 10/8/19.
+//
+
+#import "UIButton+NSTCategories.h"
+
+@implementation UIButton (NSTCategories)
+
+- (CGFloat)imageTitlePadding
+{
+    return self.titleEdgeInsets.left;
+}
+
+- (UIEdgeInsets)contentMargins
+{
+    UIEdgeInsets result = self.contentEdgeInsets;
+    result.right -= self.imageTitlePadding;
+    return result;
+}
+
+- (void)setImageTitlePadding:(CGFloat)imageTitlePadding
+{
+    [self setContentMargins:self.contentMargins imageTitlePaddings:imageTitlePadding];
+}
+
+- (void)setContentMargins:(UIEdgeInsets)contentMargins
+{
+    [self setContentMargins:contentMargins imageTitlePaddings:self.imageTitlePadding];
+}
+
+- (void)setContentMargins:(UIEdgeInsets)contentMargins imageTitlePaddings:(CGFloat)imageTitlePadding
+{
+    self.contentEdgeInsets = (UIEdgeInsets){
+        .top = contentMargins.top,
+        .left = contentMargins.left,
+        .bottom = contentMargins.bottom,
+        .right = contentMargins.right + imageTitlePadding
+    };
+    self.titleEdgeInsets = (UIEdgeInsets){
+        .top = 0,
+        .left = imageTitlePadding,
+        .bottom = 0,
+        .right = -imageTitlePadding
+    };
+}
+
+@end
