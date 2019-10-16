c51f258536cbb115100f023adbab31898d88e37a
Anatoly Shcherbinin <anatoly@netcosports.com>
2019-10-16 15:52:24 +0300

    Add UIImage+Resize

--- Extends/NSTCategories.h
+++ Extends/NSTCategories.h
@@ -8,29 +8,6 @@
 #ifndef NSTCategories_h
 #define NSTCategories_h
 
-#import "Extends+Libs.h"
-#import "NSArray+NSArray_Bundle.h"
-#import "NSArray+NSArray_Tool.h"
-#import "NSDate+NSDate_Tool.h"
-#import "NSDictionary+NSDictionary_Tool.h"
-#import "NSMutableArray+NSMutableArray_Tool.h"
-#import "NSObject+NSObject_File.h"
-#import "NSObject+NSObject_Tool.h"
-#import "NSObject+NSObject_Xpath.h"
-#import "NSString+NSString_HTML.h"
-#import "NSString+NSString_Tool.h"
-#import "NSUsefulDefines.h"
-#import "UIApplication+UIApplication_data.h"
-#import "UIColor+UIColor_Tool.h"
-#import "UIDevice+UIDevice_Tool.h"
-#import "UIImage+UIImage_Tool.h"
-#import "UILabel+UILabel_Tool.h"
-#import "UINavigationController+UINavigationController_Tool.h"
-#import "UIView+UIView_Tool.h"
-#import "UIViewController+iPhone_iPad_Xib.h"
-#import "UIViewController+UIViewController_Tool.h"
-#import "UIButton+NSTCategories.h"
-#import "UICollectionReusableView+NSTCategories.h"
-#import "UICollectionViewCell+NSTCategories.h"
+#import "NSTCategories-umbrella.h"
 
 #endif /* NSTCategories_h */
